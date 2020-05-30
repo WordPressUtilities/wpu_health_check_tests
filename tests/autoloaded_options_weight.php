255158a962c1a403bdca60def26a143a565764a3
Kevin <kevin@rocher-dev.com>
2020-05-30 22:36:56 +0200

    v 0.1.0
    Check Autoloaded options weight

---
+++ tests/autoloaded_options_weight.php
@@ -0,0 +1,55 @@
+<?php
+
+class wpu_health_check_tests__autoloaded_options_weight {
+    public function __construct() {
+        add_filter('site_status_tests', array(&$this, 'add_tests'), 10, 1);
+    }
+
+    public function add_tests($tests) {
+        $tests['direct']['wpu_health_check_tests__autoloaded_options_weight'] = array(
+            'label' => __('Autoloaded options weight', 'wpu_health_check_tests'),
+            'test' => array(&$this, 'run_test')
+        );
+        return $tests;
+    }
+
+    public function run_test() {
+        $result = array(
+            'label' => __('Autoloaded options weight is correct', 'wpu_health_check_tests'),
+            'description' => '<p>' . __('Autoloaded options should not exceed a certain weight. Your website seems ok.', 'wpu_health_check_tests') . '</p>',
+            'badge' => array(
+                'label' => __('Performance', 'wpu_health_check_tests'),
+                'color' => 'blue'
+            )
+        );
+
+        $result['status'] = $this->get_status();
+
+        if ($result['status'] == 'recommended') {
+            $result['label'] = __('Autoloaded options weight should be examined', 'wpu_health_check_tests');
+            $result['description'] = '<p>' . __('The autoloaded options may be too heavy. It could be slowing down your website', 'wpu_health_check_tests') . '</p>';
+        }
+        if ($result['status'] == 'critical') {
+            $result['label'] = __('Autoloaded options weight is too heavy', 'wpu_health_check_tests');
+            $result['description'] = '<p>' . __('The autoloaded options are too heavy. It is slowing down your website', 'wpu_health_check_tests') . '</p>';
+        }
+
+        return $result;
+    }
+
+    public function get_status() {
+        global $wpdb;
+        $autoloaded_weight = intval($wpdb->get_var("SELECT SUM(LENGTH(option_value)) as autoload_weight FROM $wpdb->options WHERE autoload='yes';"));
+        if ($autoloaded_weight < 1024) {
+            return 'good';
+        }
+
+        if ($autoloaded_weight < 2 * 1024) {
+            return 'recommended';
+        }
+
+        return 'critical';
+    }
+}
+
+$wpu_health_check_tests__autoloaded_options_weight = new wpu_health_check_tests__autoloaded_options_weight();
---
+++ wpu_health_check_tests.php
@@ -0,0 +1,22 @@
+<?php
+/*
+Plugin Name: WPU Health Check Tests
+Plugin URI: https://github.com/WordPressUtilities/wpu_health_check_tests
+Description: Add More Health Checks Tests
+Version: 0.1.0
+Author: Darklg
+Author URI: http://darklg.me/
+License: MIT License
+License URI: http://opensource.org/licenses/MIT
+*/
+
+add_action('plugins_loaded', 'wpu_health_check_tests_plugins_loaded', 10);
+function wpu_health_check_tests_plugins_loaded() {
+    if (!is_admin()) {
+        return;
+    }
+
+    load_muplugin_textdomain('wpu_health_check_tests', dirname(plugin_basename(__FILE__)) . '/lang/');
+
+    include dirname(__FILE__) . '/tests/autoloaded_options_weight.php';
+}
